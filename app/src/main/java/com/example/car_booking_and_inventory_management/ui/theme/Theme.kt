c8c2066695540c66c4413c838a2fa09686539e9e
Feben Getachew <febengetachew580@gmail.com>
2025-05-12 22:18:19 +0300

    Theme.kt

--- app/src/main/java/com/example/car_booking_and_inventory_management/ui/theme/Theme.kt
+++ app/src/main/java/com/example/car_booking_and_inventory_management/ui/theme/Theme.kt
@@ -35,7 +35,7 @@ private val LightColorScheme = lightColorScheme(
 @Composable
 fun FrontendTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
+    
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
